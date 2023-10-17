4b00ababa2dc8723f8b80bd2ec3332e6af0dc04d
kaltouma <kchbiki363@gmail.com>
2023-10-17 23:38:05 +0100

    prt

--- printf.c
+++ printf.c
@@ -10,40 +10,52 @@
 int _printf(const char *format, ...)
 {
 	int i;
+	va_list args;
 	int char_count = 0;
 
-	va_list args;
-	va_start(args, format);
+	va_start(args, format); /* Initialize the va_list */
 
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	for (i = 0; format[i]; i++)
+
+	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
 			char character = format[i];
 
-			char_count += write(1, &character, 1);
+			write(1, &character, 1);
+			char_count++;
 		}
 		else
 		{
 			i++;
-			if (format[i] == 'c' || format[i] == '%')
+			if (format[i] == 'c')
 			{
 				char character = va_arg(args, int);
 
-				char_count += write(1, &character, 1);
+				write(1, &character, 1);
+				char_count++;
 			}
 			else if (format[i] == 's')
 			{
 				char *str = va_arg(args, char *);
 
 				char_count += write(1, str, strlen(str));
+
+			}
+			else if (format[i] == '%')
+			{
+				char percent = '%';
+
+				write(1, &percent, 1);
+				char_count++;
 			}
 		}
 	}
 	va_end(args);
+
 	return (char_count);
-}	
+}
